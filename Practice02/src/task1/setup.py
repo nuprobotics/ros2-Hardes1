d6948e156e21490f1d4f68ed54680760e7c595f4
Georgii Ustinov <georgii.ustinov@jebrains.com>
2024-10-18 17:46:36 +0300

    Rename task1 to task01

--- Practice02/src/task1/setup.py
+++ Practice02/src/task1/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'task1'
+package_name = 'task01'
 
 setup(
     name=package_name,
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + "/launch", ['launch/task1.launch'])
+        ('share/' + package_name + "/launch", ['launch/task01.launch'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,7 +21,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'receiver = task1.receiver:main'
+            'receiver = task01.receiver:main'
         ],
     },
 )
