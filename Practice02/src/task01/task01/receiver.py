9dd74ce2917f647d3a536283cd3ac047e489eef6
Georgii Ustinov <georgii.ustinov@jebrains.com>
2024-10-18 17:54:34 +0300

    start spin service

--- Practice02/src/task01/task01/receiver.py
+++ Practice02/src/task01/task01/receiver.py
@@ -5,18 +5,18 @@ from std_msgs.msg import String
 class Receiver(Node):
     def __init__(self):
         super().__init__('receiver')
-        self.a = -10
         self.subscription = self.create_subscription(
             String,
             '/spgc/sender',
             self.listener_callback, 1)
 
     def listener_callback(self, msg):
-        self.get_logger().info('%s' % msg.data)
+        self.get_logger().info(f'${msg.data}')
 
 def main(args=None):
     rclpy.init(args=args)
     node = Receiver()
+    rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
