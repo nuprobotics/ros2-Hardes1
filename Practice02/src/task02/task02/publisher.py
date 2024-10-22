a52263b931cf291bf5c83f8dff4789d8f50264b7
Georgii Ustinov <georgii.ustinov@jebrains.com>
2024-10-22 20:02:14 +0300

    Disable logging

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -6,18 +6,18 @@ from std_msgs.msg import String
 class Publisher(Node):
     def __init__(self):
         super().__init__('task02')
-        self.get_logger().info("publisher started")
+        # self.get_logger().info("publisher started")
 
         self.declare_parameter('text', None)
         self.declare_parameter('topic_name', None)
 
         # Get parameters
         text = self.get_parameter('text').get_parameter_value().string_value
-        self.get_logger().info(f'text is {text}')
+         # self.get_logger().info(f'text is {text}')
         topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
-        self.get_logger().info(f'topic name is {topic_name}')
+        # self.get_logger().info(f'topic name is {topic_name}')
 
-        self.publisher_ = self.create_publisher(String, topic_name, 10)
+        self.publisher_ = self.create_publisher(String, topic_name, 1)
 
         # Set a timer to publish the message periodically
         self.timer = self.create_timer(1.0, self.publish_message)
@@ -33,7 +33,7 @@ class Publisher(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = Publisher()
-    rclpy.spin(node)
+    rclpy.spin_once(node)
     rclpy.shutdown()
 
 
