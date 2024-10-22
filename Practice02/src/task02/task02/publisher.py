6f771e0963a9c6b9dab9d3b819a445835d6b3be3
Georgii Ustinov <georgii.ustinov@jebrains.com>
2024-10-22 20:15:59 +0300

    Change run name

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -6,28 +6,20 @@ from std_msgs.msg import String
 class Publisher(Node):
     def __init__(self):
         super().__init__('publisher')
-        # self.get_logger().info("publisher started")
+        self.declare_parameter("topic_name", "")
+        self.declare_parameter("text", "")
 
-        self.declare_parameter('text', None)
-        self.declare_parameter('topic_name', None)
+        self.topic_name_parameter = self.get_parameter("topic_name").get_parameter_value().string_value
+        self.text_parameter = self.get_parameter("text").get_parameter_value().string_value
 
-        # Get parameters
-        text = self.get_parameter('text').get_parameter_value().string_value
-         # self.get_logger().info(f'text is {text}')
-        topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
-        # self.get_logger().info(f'topic name is {topic_name}')
 
-        self.publisher_ = self.create_publisher(String, topic_name, 1)
-
-        # Set a timer to publish the message periodically
+        self.publisher = self.create_publisher(String, self.topic_name_parameter, 1)
         self.timer = self.create_timer(1.0, self.publish_message)
 
-        # Store the message to publish
-        self.message = String()
-        self.message.data = text
-
     def publish_message(self):
-        self.publisher_.publish(self.message)
+        msg = String()
+        msg.data = self.text_parameter
+        self.publisher.publish(msg)
 
 
 def main(args=None):
